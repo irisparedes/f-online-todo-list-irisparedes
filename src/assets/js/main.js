beaaa67044eebb0d85d91045547d00fda17a3092
iris paredes <irisparedesjimenez@gmail.com>
2019-07-21 19:17:11 +0200

    add constants and variables. And queryselector necesary for logic

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -25,3 +25,29 @@ const allDays = [
   'Domingo',
 ];
 
+let newTask = {
+  value: '',
+  checked: false,
+  index: '',
+};
+
+let tasks = [];
+let date = new Date;
+let dayNumber = date.getDate(); //Number of the day (0-31)
+let dayOfTheWeek = date.getDay() - 1;
+let month = date.getMonth();
+let year = date.getFullYear();
+let monthName = allMonths[month]; //Name of the month
+let dayName = allDays[dayOfTheWeek]; //Name of the day
+let dayNumberContent = document.querySelector('.container__header-date-numberday');
+let dayNameContent = document.querySelector('.container__header-date-daymonthyear-day');
+let monthAndYearContent = document.querySelector('.container__header-date-daymonthyear-monthyear');
+
+const containerTasks = document.querySelector('.container__form-list');
+const addButton = document.querySelector('.add__button');
+const modalBackground = document.querySelector('.container__modal');
+const modalWindow = document.querySelector('.container__modal-window');
+const inputTask = document.querySelector('.add__task-input');
+const openModalButton = document.querySelector('.container__footer-button');
+const outsideWindow = document.querySelector('.container__modal');
+
