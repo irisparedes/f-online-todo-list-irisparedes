d1da88e7544f45d443be534e82afc8ceae56fa0d
iris paredes <irisparedesjimenez@gmail.com>
2019-07-21 19:14:04 +0200

    add arrays days of the week and month

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-console.log('>> Ready :)');
+const allMonths = [
+  'Enero',
+  'Febrero',
+  'Marzo',
+  'Abril',
+  'Mayo',
+  'Junio',
+  'Julio',
+  'Agosto',
+  'Septiembre',
+  'Octubre',
+  'Noviembre',
+  'Diciembre',
+];
+
+const allDays = [
+  'Lunes',
+  'Martes',
+  'Miércoles',
+  'Jueves',
+  'Viernes',
+  'Sábado',
+  'Domingo',
+];
+
